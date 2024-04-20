8b029c7ea58a5132de8b320234a8331670d2fed4
jonjia <jiawenhuiwar3@163.com>
2024-04-20 16:23:21 +0800

    feat: convert csv to json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, Rust!");
+use anyhow::Result;
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+
+    Ok(())
 }
